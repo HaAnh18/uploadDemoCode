26d7a9aefb71af7f117abc217bc815a818cb0647
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2024-03-07 23:41:24 +0700

    update code

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const express = require('express');
+const path = require('path');
 require('dotenv').config()
-const morgan = require('morgan');
 const mongoose = require("mongoose")
-var fileAndImageRouter = require('./routes/fileAndImage');
+const fileAndImageRouter = require('./routes/fileAndImage');
 
-var app = express();
+const app = express();
 
 //CONNECT DATABASE
 mongoose
-  .connect(process.env.DATABASE)
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log(err));
-
+.connect(process.env.DATABASE)
+.then(() => console.log("DB connected"))
+.catch((err) => console.log(err));
 
 const templatePath = path.join(__dirname, './views');
 app.use(express.json());
@@ -22,14 +19,10 @@ app.set("views", templatePath);
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.use(morgan('dev'));
-app.use(cookieParser());
-
 // Route for handling index
 app.use("/", fileAndImageRouter);
 
 const port = process.env.PORT || 8000;
-
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
